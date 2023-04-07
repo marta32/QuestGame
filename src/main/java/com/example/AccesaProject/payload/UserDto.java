d25818c328419bc371452aed7369582bdd8d7442
Marta Enachioiu <marta.enachioiu@gmail.com>
2023-04-07 21:08:17 +0300

    Add UserDto

---
+++ src/main/java/com/example/AccesaProject/payload/UserDto.java
@@ -0,0 +1,12 @@
+package com.example.AccesaProject.payload;
+
+public class UserDto {
+    private Integer id;
+    private String username;
+    private Integer tokens;
+    private String password;
+//    private List<QuestDto> proposedQuests;
+//    private List<QuestDto> resolvedQuests;
+//    private List<BudgeDto> badgeList;
+
+}
