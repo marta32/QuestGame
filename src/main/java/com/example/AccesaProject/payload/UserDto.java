c97c4c71b79b10c4e8040f5e248700fdcd63be0c
Marta Enachioiu <marta.enachioiu@gmail.com>
2023-04-07 22:07:05 +0300

    Add UserQuestDto

--- src/main/java/com/example/AccesaProject/payload/UserDto.java
+++ src/main/java/com/example/AccesaProject/payload/UserDto.java
@@ -1,12 +1,21 @@
 package com.example.AccesaProject.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
     private Integer id;
     private String username;
     private Integer tokens;
     private String password;
-//    private List<QuestDto> proposedQuests;
-//    private List<QuestDto> resolvedQuests;
-//    private List<BudgeDto> badgeList;
+//    private List<QuestUserDto> proposedQuests;
+//    private List<QuestUserDto> resolvedQuests;
+//    private List<BudgeUserDto> badgeList;
 
 }
