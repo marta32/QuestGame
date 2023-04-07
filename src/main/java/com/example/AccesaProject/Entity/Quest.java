a5775178670af0222539e05776ad706b88ed196c
Marta Enachioiu <marta.enachioiu@gmail.com>
2023-04-07 18:01:06 +0300

    Add Quest Entity

--- src/main/java/com/example/AccesaProject/Entity/Quest.java
+++ src/main/java/com/example/AccesaProject/Entity/Quest.java
@@ -1,5 +1,27 @@
 package com.example.AccesaProject.Entity;
 
-public class Quest {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="quests")
+public class Quest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String quest;
+    @Column
+    private String answear;
+    @Column
+    private Integer tokens;
+    @Column
+    private String status;
 }
