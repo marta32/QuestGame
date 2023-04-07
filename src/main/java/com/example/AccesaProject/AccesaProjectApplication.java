fa5664a71343c13017a41134fe2552b45aa5bf77
Marta Enachioiu <marta.enachioiu@gmail.com>
2023-04-07 16:34:05 +0300

    Add project setup

---
+++ src/main/java/com/example/AccesaProject/AccesaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccesaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AccesaProject/AccesaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccesaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
